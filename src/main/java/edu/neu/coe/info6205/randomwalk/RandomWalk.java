72f095f028c828cb57750b73a10ac8bb506797e6
Robin Hillyard <scalaprof@gmail.com>
2018-01-15 23:26:59 -0500

    Update RandomWalk.java
    Provide more of a skeleton code for assignment 1

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -4,19 +4,35 @@
 
 package edu.neu.coe.info6205.randomwalk;
 
+import java.util.Random;
+
 public class RandomWalk {
     private int x = 0;
     private int y = 0;
 
-    public void move(int dx, int dy) {
+    private final Random random = new Random();
 
+    public void move(int dx, int dy) {
+        // TODO you need to implement this
     }
 
-    public void randomWalk(int n) {
-        // TODO perform n moves
+    private void randomWalk(int n) {
+        for (int i = 0; i < n; i++)
+            randomMove();
+    }
+
+    private void randomMove() {
+        // TODO you need to implement this
     }
 
     public double distance() {
-        return 0.0;  // TODO you need to implement this
+        return 0; // TODO you need to implement this
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        RandomWalk walk = new RandomWalk();
+        walk.randomWalk(n);
+        System.out.println(n + " steps: " + walk.distance());
     }
 }
