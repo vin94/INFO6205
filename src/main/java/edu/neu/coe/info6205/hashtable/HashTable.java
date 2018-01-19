3742c6ec7834428b689be6ac4d19913c9d96af3c
yingyuan <yingy4@hotmail.com>
2018-01-19 16:01:39 -0500

    HashTable: add empty method

--- src/main/java/edu/neu/coe/info6205/hashtable/HashTable.java
+++ src/main/java/edu/neu/coe/info6205/hashtable/HashTable.java
@@ -1,4 +1,26 @@
 package edu.neu.coe.info6205.hashtable;
 
 public class HashTable {
+
+    int size;
+
+    public HashTable(int capacity) {
+    }
+
+    public void put(Object key, Object value) {
+    }
+
+    public Object getValue(Object key) {
+        return null;
+    }
+
+    // This is only for testing and should be made private
+    public boolean check(int bits, int length) {
+        return false;
+    }
+
+    public void show() {
+
+    }
+
 }
