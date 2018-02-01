0de1ebfef473ad04efa639623e43197891f5869d
Scala Prof <scalaprof@gmail.com>
2018-02-01 17:11:31 -0500

    Added sort/simple package and tests
    (cherry picked from commit 556f8061d19c6f7292fc014732140f7cf80f63a2)

---
+++ src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -0,0 +1,10 @@
+package edu.neu.coe.info6205.sort.simple;
+
+import static edu.neu.coe.info6205.sort.simple.Helper.*;
+
+public class InsertionSort<X> implements Sort<X> {
+    @Override
+    public void sort(Comparable<X>[] xs) {
+        // TODO implement selection sort
+    }
+}
---
+++ src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java
@@ -0,0 +1,11 @@
+package edu.neu.coe.info6205.sort.simple;
+
+import static edu.neu.coe.info6205.sort.simple.Helper.less;
+import static edu.neu.coe.info6205.sort.simple.Helper.swap;
+
+public class SelectionSort<X> implements Sort<X> {
+    @Override
+    public void sort(Comparable<X>[] xs) {
+        // TODO implement selection sort
+    }
+}
