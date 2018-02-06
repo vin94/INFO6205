cf2eb9d43b0e5275adfa50d1f13cfadcc2c78736
Scala Prof <scalaprof@gmail.com>
2018-02-06 13:38:34 -0500

    Fixed off-by-one error in new version of Sort; Some reformating of code.
    (cherry picked from commit c0efc84146ed8695d16f3c8d231b5d76fe407efe)
    
    # Conflicts:
    #	src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
    #	src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java

--- src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java
@@ -4,6 +4,7 @@ import static edu.neu.coe.info6205.sort.simple.Helper.less;
 import static edu.neu.coe.info6205.sort.simple.Helper.swap;
 
 public class SelectionSort<X> implements Sort<X> {
+
     @Override
     public void sort(Comparable<X>[] xs, int from, int to) {
         // TODO implement selection sort
