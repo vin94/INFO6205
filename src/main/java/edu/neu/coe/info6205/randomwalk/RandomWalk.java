231dc21ca2a0a25466a4f243ff0ac8840ff68bbb
rhillyard <scalaprof@gmail.com>
2018-09-18 14:46:11 -0400

    Cosmetics for Queue_Elements, RandomWalk and Stack_LinkedList
    (cherry picked from commit 68eeec5)

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -7,19 +7,38 @@ package edu.neu.coe.info6205.randomwalk;
 import java.util.Random;
 
 public class RandomWalk {
+
     private int x = 0;
     private int y = 0;
 
     private final Random random = new Random();
 
+    /**
+     * Private method to move the current position, that's to say the drunkard moves
+     * @param dx the distance he moves in the x direction
+     * @param dy the distance he moves in the y direction
+     */
     private void move(int dx, int dy) {
         // TODO you need to implement this
     }
 
     public void randomWalk(int n) {
         // TODO perform n moves
+    /**
+     * Private method to generate a random move according to the rules of the situation.
+     * That's to say, moves can be (+-1, 0) or (0, +-1).
+     */
+    private void randomMove() {
+        boolean ns = random.nextBoolean();
+        int step = random.nextBoolean() ? 1 : -1;
+        move(ns ? step : 0, ns ? 0 : step);
     }
 
+    /**
+     * Method to compute the distance from the origin (the lamp-post where the drunkard starts) to his current position.
+     *
+     * @return the (Euclidean) distance from the origin to the current position.
+     */
     public double distance() {
         return 0; // TODO you need to implement this
     }
