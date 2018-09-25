9b6c8f295aeb8d3fa5a8f15bdc819bd69385ea46
rhillyard <scalaprof@gmail.com>
2018-09-25 17:04:55 -0400

    Updated FrequencyCounter
    (cherry picked from commit 0c12ac9)
    
    (cherry picked from commit 50c661c)

--- src/main/java/edu/neu/coe/info6205/hashtable/FrequencyCounter.java
+++ src/main/java/edu/neu/coe/info6205/hashtable/FrequencyCounter.java
@@ -5,17 +5,16 @@
 package edu.neu.coe.info6205.hashtable;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FrequencyCounter<K> {
 
-    private final ST<K, Integer> map;
-
-    public FrequencyCounter(ST<K, Integer> map) {
+    public FrequencyCounter(ST<K,Integer> map) {
         this.map = map;
     }
 
     public FrequencyCounter() {
-        this(new STMap<>(new HashMap<>()));
+        this(new STMap<K,Integer>(new HashMap<>()));
     }
 
     public int get(Object key) {
@@ -33,9 +32,10 @@ public class FrequencyCounter<K> {
         return map.toString();
     }
 
+    private final ST<K, Integer> map;
+
     public static void main(String[] args) {
         FrequencyCounter<String> counter = new FrequencyCounter<>();
-
         for (String arg : args) counter.increment(arg);
 
         System.out.println(counter.toString());
