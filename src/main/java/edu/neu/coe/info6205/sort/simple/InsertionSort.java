1da081b517912f101deae175f4486c1990fc2b21
rhillyard <scalaprof@gmail.com>
2018-10-03 14:50:21 -0400

    Temporary commit while working on Sort signatures.
    (cherry picked from commit 938b2e0)
    
    (cherry picked from commit f23fdb0)

--- src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/InsertionSort.java
@@ -1,9 +1,11 @@
 package edu.neu.coe.info6205.sort.simple;
 
+import static edu.neu.coe.info6205.sort.simple.Helper.less;
+import static edu.neu.coe.info6205.sort.simple.Helper.swap;
+
 public class InsertionSort<X extends Comparable<X>> implements Sort<X> {
     @Override
-    public X[] sort(X[] xs, int from, int to) {
+    public void sort(X[] xs, int from, int to) {
         // TODO implement insertionSort
-        return null;
     }
 }
